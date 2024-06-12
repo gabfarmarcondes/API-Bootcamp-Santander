7644799e39fc48fa25e1a8b08deee54865bf0bc0
gabfarmarcondes <gabriel.fmarcondes@hotmail.com>
2024-06-12 14:52:55 -0300

    Criação do Package Model e criando as suas respectivas classes

---
+++ src/main/java/me/dio/model/User.java
@@ -0,0 +1,11 @@
+package me.dio.model;
+
+import java.util.List;
+
+public class User {
+    private Long id;
+    private String name;
+    private Account account;
+    private List<Feature> features;
+    private List<News> news;
+}
